354928e9c5c8d3a105323999e8394c360e80e0b5
Brian Hernandez <Brian@Hernandez-cs.com>
2015-04-04 08:01:58 -0400

    Methods for HW3 added.

---
+++ HW3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+class bookCase
+{
+public:
+    void empty();
+    int numberOfDistinctItems();
+    int count(int id);
+    void add(int id);
+    void remove(int id);
+    bool pickAnItem(int &id);
+private:
+    int bookIDs[];
+}
+
+void bookCase::empty()
+{
+}
+
+int bookCase::numberOfDistinctItems()
+{
+}
+
+int bookCase::count(int id)
+{
+}
+
+void bookCase::add(int id)
+{
+}
+
+void bookCase::remove(int id)
+{
+}
+
+bool pickAnItem(int &id)
+{
+}
